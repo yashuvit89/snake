6be55e3adb7baabf1a7908b4e366678f756994a4
Yaswanth <yaswanth@Yaswanth.local>
2016-10-24 00:01:34 +0530

    travis config for npm build

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.state.speed}</h1>
+        <h1>Hello World! {this.state.speed}</h1>
       </div>
     );
   }
